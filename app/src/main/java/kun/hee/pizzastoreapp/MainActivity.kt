68f3454d4785bd607331c90d2d3e7a81b9a7b121
smlrkh1107 <krae7789230@gmail.com>
2020-04-22 21:13:51 +0900

    피자가게 정보를 넣고 list view에 뿌려줌.

--- app/src/main/java/kun/hee/pizzastoreapp/MainActivity.kt
+++ app/src/main/java/kun/hee/pizzastoreapp/MainActivity.kt
@@ -2,10 +2,16 @@ package kun.hee.pizzastoreapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.store_info.*
+import kun.hee.pizzastoreapp.adapters.StoreAdapter
 import kun.hee.pizzastoreapp.datas.Store
 
 class MainActivity : BaseActivity() {
+
+    var mPizzaAdapter:StoreAdapter? = null
+    val pizzaStores = ArrayList<Store>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,7 +28,19 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-        val tempSotre = Store("피자헛","naver.com","123-5678")
+        addPizzaStores()
+        mPizzaAdapter = StoreAdapter(mContext, R.layout.store_list, pizzaStores)
+
+        storeListView.adapter = mPizzaAdapter
+
+    }
+
+    fun addPizzaStores() {
+
+        pizzaStores.add(Store("피자헛", "02-999-9999","https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))
+        pizzaStores.add(Store("파파존스", "02-222-0101","http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800"))
+        pizzaStores.add(Store("도미노 피자", "02-888-9787","https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200"))
+        pizzaStores.add(Store("미스터 피자", "02-xxx-xxxx","https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png"))
 
 
     }
--- app/src/main/java/kun/hee/pizzastoreapp/datas/Store.kt
+++ app/src/main/java/kun/hee/pizzastoreapp/datas/Store.kt
@@ -1,3 +1,3 @@
 package kun.hee.pizzastoreapp.datas
 
-data class Store (val name:String="미입력", val logoUrl:String, val StoreNum:String)
+data class Store (val name:String="미입력", val StoreNum:String, val logoUrl:String)
