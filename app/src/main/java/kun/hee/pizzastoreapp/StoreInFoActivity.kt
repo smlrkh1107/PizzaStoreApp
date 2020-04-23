17b5adcb28bd541cba38f5f9e3286f20e781c9e1
smlrkh1107 <krae7789230@gmail.com>
2020-04-23 20:06:02 +0900

    전화버튼 누르면 권한 확인 창 실행

--- app/src/main/java/kun/hee/pizzastoreapp/StoreInFoActivity.kt
+++ app/src/main/java/kun/hee/pizzastoreapp/StoreInFoActivity.kt
@@ -1,8 +1,11 @@
 package kun.hee.pizzastoreapp
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.store_info.*
 import kun.hee.pizzastoreapp.adapters.StoreAdapter
 import kun.hee.pizzastoreapp.datas.Store
@@ -24,10 +27,27 @@ class StoreInFoActivity : BaseActivity() {
 
 
         callBtn.setOnClickListener {
+//            it 변수 활용 예시.
+//             it.visibility = view.GONE
+            val pl = object : PermissionListener{
+                override fun onPermissionGranted() {
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                }
+
+
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("통화 권한 설정을 해야 사용 가능합니다. [설정]에서 권한을 허용해주세요.")
+                .setPermissions(Manifest.permission.CALL_PHONE) //안드로이드가 주는 manifest를 꼭 import
+                .check()
 
         }
-
-
 
     }
 
